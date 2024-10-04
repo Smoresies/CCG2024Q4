847ee4f860dc599e764226b3c16ab33199b3781c
aaronspringer <ajspringer05@gmail.com>
2024-10-03 18:30:39 -0600

    Updated in response to pull request

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -4,28 +4,41 @@ class_name Player extends CharacterBody2D
 @export var MAX_SPEED: int = 200
 @export var JUMP_VELOCITY: int = -300.0
 @export var ACCELERATION: int = 1500
-@export var DECELERATION_TIME: float = 0.2
+@export_range(0, 10, 0.1, "or_greater") var DECELERATION_TIME: float = 0.2
 
 var deceleration: float = MAX_SPEED / DECELERATION_TIME
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	apply_gravity(delta)
+	
+	player_jump()
+	
+	horizontal_movement(delta)
+
+	move_and_slide()
+
+
+func apply_gravity(delta: float):
+	# Add the apply_gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+func player_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+func horizontal_movement(delta: float):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction:
+
+	
+	if direction: # Direction button active
 		# Accelerate towards the target speed
 		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
-	else:
-		# Decelerate towards zero speed
-		var deceleration_rate = (MAX_SPEED / DECELERATION_TIME)
-		velocity.x = move_toward(velocity.x, 0, deceleration_rate * delta)
 
-	move_and_slide()
+	else: # Direction button inactive
+		# Decelerate towards zero speed
+		print(deceleration)
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
+	
