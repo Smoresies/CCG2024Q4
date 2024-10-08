0c6af774461b3c83294c832cbe1d35a3ee797a7a
Oliver Mills <69769204+SllimRevilo@users.noreply.github.com>
2024-10-07 22:00:16 -0700

    Added comments

--- scripts/entity/components/movement_components/player_movement_component.gd
+++ scripts/entity/components/movement_components/player_movement_component.gd
@@ -1,12 +1,18 @@
 class_name PlayerMovementComponent extends BaseMovementComponent
+## The movement implementation for a human player.
 
+## The max horizontal speed the entity can move.
 @export var MAX_SPEED: int = 200
+## The jump velocity of the entity.
 @export var JUMP_VELOCITY: int = -300
+## The horizontal movement acceleration for the entity.
 @export var ACCELERATION: int = 1500
+## The time it takes to fully decelerate from horizontal input in seconds.
 @export_range(0, 10, 0.1, "or_greater") var DECELERATION_TIME: float = 0.2
 
-var current_direction: float = 0
-
+## The current input direction.
+var current_input_direction: float = 0
+## How fast the character decelerates after loss of input.
 var deceleration: float = MAX_SPEED / DECELERATION_TIME
 
 # Called when the node enters the scene tree for the first time.
@@ -14,49 +20,43 @@ func _ready() -> void:
 	#do nothing
 	pass
 
+## Applies movement to the character.
 func _physics_process(delta: float) -> void:
-	apply_gravity(delta)
-		
+	super._physics_process(delta)
+	apply_gravity(delta)	
 	apply_horizontal_movement(delta)
-
 	move_and_slide()
 
-func apply_gravity(delta: float):
-	# Add the apply_gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func on_movement_input(horizontal_vector2: float) -> void:
+	if(current_input_direction == 0 && horizontal_vector2 != 0):
+		on_horizontal_movement_started.emit(horizontal_vector2)
+	elif(current_input_direction != 0 && horizontal_vector2 == 0):
+		on_horizontal_movement_cancelled.emit(horizontal_vector2)
+	elif current_input_direction != 0 && horizontal_vector2 != 0:
+		on_horizontal_movement.emit(horizontal_vector2)
+	
+	current_input_direction = horizontal_vector2
 
-func on_movement_started(horizontal_vector2: float) -> void:
-	# do nothing
-	pass
-
-func on_movement(horizontal_vector2: float) -> void:
-	current_direction = horizontal_vector2
-
-func on_movement_cancelled(horizontal_vector2: float) -> void:
-	# do nothing
-	pass
-
-func on_jump_started() -> void:
+func on_jump_input_started() -> void:
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-func on_jump() -> void:
+func on_jump_input() -> void:
 	# do nothing
 	pass
 
-func on_jump_cancelled() -> void:
+func on_jump_input_cancelled() -> void:
 	# do nothing
 	pass
 	
-
+## Moves the entity based on the current input direction.
 func apply_horizontal_movement(delta: float):
-	if current_direction: # Direction button active
+ 	# Direction button active
+	if current_input_direction:
 		# Accelerate towards the target speed
-		velocity.x = move_toward(velocity.x, current_direction * MAX_SPEED, ACCELERATION * delta)
-
-	else: # Direction button inactive
+		velocity.x = move_toward(velocity.x, current_input_direction * MAX_SPEED, ACCELERATION * delta)
+	 # Direction button inactive
+	else:
 		# Decelerate towards zero speed
-		print(deceleration)
 		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 	
