51404d8606cd293b1f71ac0a042d0fc04e3dbd0b
Ethan Kelly <ekellysnoco@gmail.com>
2024-10-10 16:21:36 -0700

    Final draft: initial footsteps implementation
    Global audio manager (node) -> (resource)
    Deleted player movement script

--- scripts/global_audio_manager.gd
+++ scripts/global_audio_manager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Resource
 
 @export var master_volume: int
 @export var music_volume: int
@@ -6,6 +6,7 @@ extends Node
 @export var ambience_volume: int
 
 # Called when the node enters the scene tree for the first time.
+# Sets volume to a reasonable default with room to adjust up and down
 func _ready() -> void:
 	set_master_volume(8)
 	set_music_volume(8)
@@ -15,14 +16,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
+
+# TODO
 func set_master_volume(volume: int):
 	pass
-	
+
+# TODO
 func set_music_volume(volume: int):
 	pass
-	
+
+# TODO
 func set_sfx_volume(volume: int):
 	pass
-	
-	
