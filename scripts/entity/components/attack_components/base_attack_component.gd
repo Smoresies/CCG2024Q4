c3eb03536705b5b3648b8169741cce4116266bea
Oliver Mills <69769204+SllimRevilo@users.noreply.github.com>
2024-10-20 12:34:09 -0700

    making more changes

--- scripts/entity/components/attack_components/base_attack_component.gd
+++ scripts/entity/components/attack_components/base_attack_component.gd
@@ -15,19 +15,27 @@ signal on_target_change(previous_target, current_target)
 ## Used to continue a coroutine for updating the target.
 var _update_targets: bool = true
 
+## The number of current targets.
+var _num_targets: int = 0
+
 ## The current target that is closest in the area.
 var _current_target: Node2D = null
 
+## If the update target timer is running.
+var _update_target_timer_running: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	lock_on_area.body_entered.connect(_add_target)
+	lock_on_area.body_exited.connect(_remove_target)
 	on_target_change.connect(target_changed)
-	update_closest_target()
 
 
 ## Checks each target in the lock on area and then determines which is the closest. Emits an on_target_change signal if the target has changed.
 func update_closest_target():
-	while _update_targets:
-		print("WAT")
+	# Run while 
+	while (_update_targets || _current_target != null) && !_update_target_timer_running:
+		_update_target_timer_running = true
 		var closest_node: Node2D = null
 		var closest_distance: float =  INF
 
@@ -44,12 +52,24 @@ func update_closest_target():
 			on_target_change.emit(_current_target, closest_node)
 			_current_target = closest_node
 
-		await(get_tree().create_timer(time_between_target_updates_in_seconds).timeout)
+		# Continue when we have waited the timeout duration
+		await(get_tree().create_timer(time_between_target_updates_in_seconds, false, true, false).timeout)
+		_update_target_timer_running = false
 
 ## Used for debug purposes only.
 func target_changed(previous_target, curr_target):
 	print("prev target: " + str(previous_target))
 	print("curr target: " + str(curr_target))
 
-func _exit_tree() -> void:
-	_update_targets = false
+
+## Adds a target and starts the update_closest_target coroutine.
+func _add_target(_body: Node2D):
+	_num_targets += 1
+	if _num_targets == 1:
+		_update_targets = true
+		update_closest_target()
+
+## Removes a target and updates the boolean for continuing running the coroutine.
+func _remove_target(_body: Node2D):
+	_num_targets -= 1
+	_update_targets = _num_targets > 0
