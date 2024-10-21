34c7c89cd559ab0acc88f1b0daba3c98ffa7f5ff
aaronspringer <ajspringer05@gmail.com>
2024-10-20 18:07:17 -0600

    Completed CCG-80 and CCG-81

--- scripts/entity/components/attack_components/base_attack_component.gd
+++ scripts/entity/components/attack_components/base_attack_component.gd
@@ -10,21 +10,21 @@ signal on_attack()
 signal on_attack_cancelled()
 
 func _ready() -> void:
-    # Abstract class instantiation error
-    push_error('Abstract Class Instantiation Error: %s' % [name])
-    assert(false, 'Abstract Class Instantiation Error: %s' % [name])
+	# Abstract class instantiation error
+	push_error('Abstract Class Instantiation Error: %s' % [name])
+	assert(false, 'Abstract Class Instantiation Error: %s' % [name])
 
 func on_attack_input_started() -> void:
-    # Abstract method error
-    push_error('Abstract Method Not Implemented Error: %s' % [name])
-    assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
+	# Abstract method error
+	push_error('Abstract Method Not Implemented Error: %s' % [name])
+	assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
 
 func on_attack_input() -> void:
-    # Abstract method error
-    push_error('Abstract Method Not Implemented Error: %s' % [name])
-    assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
+	# Abstract method error
+	push_error('Abstract Method Not Implemented Error: %s' % [name])
+	assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
 
 func on_attack_input_cancelled() -> void:
-    # Abstract method error
-    push_error('Abstract Method Not Implemented Error: %s' % [name])
-    assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
+	# Abstract method error
+	push_error('Abstract Method Not Implemented Error: %s' % [name])
+	assert(false, 'Abstract Method Not Implemented Error: %s' % [name])
--- scripts/entity/components/attack_components/player_attack_component.gd
+++ scripts/entity/components/attack_components/player_attack_component.gd
@@ -1,17 +1,21 @@
 class_name PlayerAttackComponent extends BaseAttackComponent
 
-@export var projectile_scene: PackedScene  # The projectile scene to instantiate
-
+@export var projectile_scene: PackedScene # The projectile scene to instantiate
+@export var projectile_spawn_position: Node2D
 func _ready() -> void:
-    # Override the abstract class
-    pass
+	# Override the abstract class
+	pass
 
 func on_attack_input_started() -> void:
-    # Create an instance of the projectile
-    var projectile_instance = projectile_scene.instantiate()
-    
-    # Set the position to the player's position
-    projectile_instance.init(5, [1], 0, 200)
-    
-    # Add the projectile to the scene tree
-    get_tree().current_scene.add_child(projectile_instance)
+	# Create an instance of the projectile
+	var projectile_instance: BaseProjectileComponent = projectile_scene.instantiate()
+	add_child(projectile_instance)
+	# Initialize the projectile
+	projectile_instance.init(projectile_spawn_position.global_position, 5, [1], 0, 200, Vector2.ZERO)
+	print("attacked")
+
+func on_attack_input() -> void:
+	pass
+
+func on_attack_input_cancelled() -> void:
+	pass
