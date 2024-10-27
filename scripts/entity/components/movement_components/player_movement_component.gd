754afda73ce3b2e922e2fc5849d5c8e934478ff7
aaronspringer <ajspringer05@gmail.com>
2024-10-27 12:25:55 -0600

    Update

--- scripts/entity/components/movement_components/player_movement_component.gd
+++ scripts/entity/components/movement_components/player_movement_component.gd
@@ -1,10 +1,14 @@
 class_name PlayerMovementComponent extends BaseMovementComponent
 ## The movement implementation for a human player.
 
+signal on_moth_jump()
+
+signal on_moth_jump_ended()
+
 ## The max horizontal speed the entity can move.
 @export var MAX_SPEED: int = 200
 ## The jump velocity of the entity.
-@export var JUMP_VELOCITY: int = -350
+@export var JUMP_VELOCITY: int = 350
 ## The horizontal movement acceleration for the entity.
 @export var ACCELERATION: int = 1500
 ## The time it takes to fully decelerate from horizontal input in seconds.
@@ -12,7 +16,7 @@ class_name PlayerMovementComponent extends BaseMovementComponent
 
 ## Moth Jump variables
 @export var MOTH_JUMP_HANGTIME: float = 1.0
-@export var MOTH_JUMP_DIP_AMOUNT: float = -18.0
+@export var MOTH_JUMP_DIP_AMOUNT: float = 18.0
 @export var MOTH_JUMP_FLUTTER_DURATION: float = 0.35
 
 ## The current input direction.
@@ -30,6 +34,8 @@ var _is_jump_button_held: bool = false
 
 
 func _ready() -> void:
+	JUMP_VELOCITY *= -1
+	MOTH_JUMP_DIP_AMOUNT *= -1
 	pass
 
 ## Applies movement to the character.
@@ -48,12 +54,9 @@ func on_jump_input_started() -> void:
 		on_jump_started.emit()
 	elif not _is_moth_jumping:
 		# Start Moth Jump
-		_is_moth_jumping = true
-		_is_fluttering = true
-		_moth_jump_timer = 0.0
-		_moth_jump_flutter_timer = 0.0
-		_moth_jump_initial_height = global_position.y
-		_is_jump_button_held = true
+		_init_moth_jump()
+		
+		
 
 func on_jump_input() -> void:
 	_is_jump_button_held = true
@@ -73,27 +76,46 @@ func apply_horizontal_movement(delta: float) -> void:
 func apply_gravity(delta: float) -> void:
 	if _is_moth_jumping:
 		if _is_fluttering:
-			# Handle the flutter dip
-			var flutter_progress: float = _moth_jump_flutter_timer / MOTH_JUMP_FLUTTER_DURATION
-			# Copy in to desmos to see why I chose this function: -\sin\left(\frac{\left(x-a\right)^{2}}{c}\right)+1
-			var flutter_dip: float = -MOTH_JUMP_DIP_AMOUNT * (-sin((pow((flutter_progress-0.5),2))/0.16) + 1)
-			var desired_position_y: float = _moth_jump_initial_height + flutter_dip
-			var position_difference_y: float = desired_position_y - global_position.y
-			velocity.y = position_difference_y / delta
+			
+			_apply_moth_jump_dip(delta)
+			
 			_moth_jump_flutter_timer += delta
 			if _moth_jump_flutter_timer >= MOTH_JUMP_FLUTTER_DURATION:
 				_is_fluttering = false
-				global_position.y = _moth_jump_initial_height
 				velocity.y = 0.0
+				on_moth_jump_ended.emit()
+
 		else:
 			if _is_jump_button_held and _moth_jump_timer < MOTH_JUMP_HANGTIME:
 				# Maintain the current height
 				velocity.y = 0.0
 				_moth_jump_timer += delta
+
 			else:
 				# End Moth Jump
 				_is_moth_jumping = false
-				velocity.y += super.get_gravity().y * delta  # Resume normal gravity
+				super.apply_gravity(delta)  # Resume normal gravity
+
 	else:
 		# Apply normal gravity
-		velocity.y += super.get_gravity().y * delta
+		super.apply_gravity(delta)
+
+
+func _init_moth_jump() -> void:
+	_is_moth_jumping = true
+	_is_fluttering = true
+	_moth_jump_timer = 0.0
+	_moth_jump_flutter_timer = 0.0
+	_moth_jump_initial_height = global_position.y
+	_is_jump_button_held = true
+	on_moth_jump.emit()
+	
+	
+func _apply_moth_jump_dip(delta: float) -> void:
+	# Handle the flutter dip
+	var flutter_progress: float = _moth_jump_flutter_timer / MOTH_JUMP_FLUTTER_DURATION
+	# Copy in to desmos to see why I chose this function: -\sin\left(\frac{\left(x-a\right)^{2}}{c}\right)+1
+	var flutter_dip: float = -MOTH_JUMP_DIP_AMOUNT * (-sin((pow((flutter_progress-0.5),2))/0.16) + 1)
+	var desired_position_y: float = _moth_jump_initial_height + flutter_dip
+	var position_difference_y: float = desired_position_y - global_position.y
+	velocity.y = position_difference_y / delta
