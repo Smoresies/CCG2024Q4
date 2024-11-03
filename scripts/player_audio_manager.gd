1249851d38b5abc5e22e844077f2fb4a65265a8f
Ethan Kelly <ekellysnoco@gmail.com>
2024-11-03 10:50:31 -0800

    Updated player audio manager with checks
    Now checks for available stream playback before setting the material to default. On fail, returns -1

--- scripts/player_audio_manager.gd
+++ scripts/player_audio_manager.gd
@@ -12,15 +12,20 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_sfx_floor_material(default_sfx_floor_material)
+	if set_sfx_floor_material(default_sfx_floor_material) < 0:
+		print("No stream playback found")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 # Set the floor material beneath the player for triggering SFX. 
-# Invalid inputs will reset material to default_sfx_floor_material
-func set_sfx_floor_material(sfx_material: String) -> void:
+# Invalid inputs will reset material to default_sfx_floor_material.
+# If the floor material has no stream playback, returns -1. 
+func set_sfx_floor_material(sfx_material: String) -> int:
+	if !(footstep_sfx.has_stream_playback() && jumping_sfx.has_stream_playback() && landing_sfx.has_stream_playback()):
+		return -1
+		
 	# If the input is valid, accept the new material
 	if sfx_material in sfx_floor_materials:
 		current_sfx_material = sfx_material
@@ -28,10 +33,11 @@ func set_sfx_floor_material(sfx_material: String) -> void:
 	else: 
 		current_sfx_material = default_sfx_floor_material
 		
-	# Apply the new/default SFX material to relevant SFX player nodes.  
+	# Apply the new/default SFX material to relevant SFX player nodes. 
 	footstep_sfx.get_stream_playback().switch_to_clip_by_name(current_sfx_material)
 	jumping_sfx.get_stream_playback().switch_to_clip_by_name(current_sfx_material)
 	landing_sfx.get_stream_playback().switch_to_clip_by_name(current_sfx_material)
+	return 1
 
 # When the moth jump is released, 
 func _on_moth_jump_released() -> void:
