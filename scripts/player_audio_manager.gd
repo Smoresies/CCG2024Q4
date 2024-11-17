1993ad33115492c3a64f2e264110aef854ecc3cb
Ethan Kelly <55097687+ekellysnoco@users.noreply.github.com>
2024-11-17 14:00:22 -0800

    Ccg 86 implement sound effects for firing weapons (#19)
    * Imported SFX
    
    * Created player child node to play back firing sounds. Removed duplicate imports
    
    * Updated bus routing for audiostreamplayers (Master > SFX)
    
    * Imported travel and destroy SFX to Projectile prefab
    
    * Removed ready function that caused crashes. Made weapon firing unique to fix footsteps.
    
    * Tuned footstep SFX timing
    
    * Updated timing of firing animation playback
    
    * Updated comments to be more accurate
    
    * Fixed issues relating to deprecated SFX from duplicated nodes

--- scripts/player_audio_manager.gd
+++ scripts/player_audio_manager.gd
@@ -4,19 +4,20 @@ extends Node
 @export var footstep_sfx: AudioStreamPlayer2D
 @export var jumping_sfx: AudioStreamPlayer2D
 @export var moth_jump_sfx: AudioStreamPlayer2D
+@onready var weapon_firing_sfx: AudioStreamPlayer2D = $"Weapon Firing SFX"
+
 # An array containing the valid surface types for sound effects. 
 @export var sfx_floor_materials: Array = [&"dirt", &"wood"]
 # The first item in the array is the default value.
-# Changed to be permanent until crashing errors are solved
 var default_sfx_floor_material: StringName = sfx_floor_materials[0]
 var current_sfx_material: String = default_sfx_floor_material
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	set_sfx_floor_material(default_sfx_floor_material)
+# An array containing supported weapon SFX types. 
+@export var weapon_sfx_types: Array = [&"basic pellet"]
+var default_weapon_sfx: String = weapon_sfx_types[0]
+var current_weapon_sfx: String = default_weapon_sfx
 
-# Set the floor material beneath the player for triggering SFX. 
-# Invalid inputs will reset material to default_sfx_floor_material
+## Set the floor material beneath the player for triggering SFX. Invalid inputs will reset material to the default value. 
 func set_sfx_floor_material(sfx_material: String) -> void:
 	# If the input is valid, accept the new material
 	if sfx_material in sfx_floor_materials:
@@ -26,7 +27,6 @@ func set_sfx_floor_material(sfx_material: String) -> void:
 		current_sfx_material = default_sfx_floor_material
 		
 	# Apply the new/default SFX material to relevant SFX player nodes. 
-	### CURRENTLY HAS A TEMPORARY IF. THIS STOPS A CRASH. NEED TO PLAY FIRST 
 	if footstep_sfx.has_stream_playback():
 		footstep_sfx.get_stream_playback().switch_to_clip_by_name(current_sfx_material)
 	if jumping_sfx.has_stream_playback():
@@ -34,6 +34,18 @@ func set_sfx_floor_material(sfx_material: String) -> void:
 	if landing_sfx.has_stream_playback():
 		landing_sfx.get_stream_playback().switch_to_clip_by_name(current_sfx_material)
 
-# When the moth jump is released, 
+## When the moth jump is released, switch the active clip to wind down the sound effect. 
 func _on_moth_jump_released() -> void:
 	moth_jump_sfx.get_stream_playback().switch_to_clip_by_name("Player Moth-jump Release")
+
+## Update weapon-related sound effects by passing in the name of the active weapon modifier. 
+func set_weapon_sfx_type(weapon_sfx: String) -> void:
+	if weapon_sfx in weapon_sfx_types:
+		current_weapon_sfx = weapon_sfx
+	else:
+		current_weapon_sfx = default_weapon_sfx
+	
+	# Apply the new weapon SFX type to AudioStreamPlayer node.
+	if weapon_firing_sfx.has_stream_playback():
+		weapon_firing_sfx.get_stream_playback().switch_to_clip_by_name(current_weapon_sfx)
+	# TODO: check and update projectile travel and destroy SFX with new weapon SFX 
