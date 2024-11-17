46258ce8fec90b8efc00abe778fa67381bb2f8c8
Oliver Mills <69769204+SllimRevilo@users.noreply.github.com>
2024-11-17 14:12:15 -0800

    moving more things out

--- scripts/entity/base_entity.gd
+++ scripts/entity/base_entity.gd
@@ -27,10 +27,3 @@ func _ready() -> void:
 		input_component.on_attack_input_started.connect(attack_component.on_attack_input_started)
 		input_component.on_attack_input.connect(attack_component.on_attack_input)
 		input_component.on_attack_input_cancelled.connect(attack_component.on_attack_input_cancelled)
- 
-
-		input_component.on_jump_input_started.connect(make_terrain)
-
-func make_terrain():
-	if terrain_generator:
-		terrain_generator.generate_dungeon(self.get_parent())
