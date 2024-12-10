4afca0d7b37ac952ded842746e9d5041adce4794
Smoresies <gavin.britton@gmail.com>
2024-12-10 11:23:18 -0800

    basic main menu set up

---
+++ scenes/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
