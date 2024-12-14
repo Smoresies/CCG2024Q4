fd95e5a732e30831bd42959424cdea9653aaa639
Ethan Kelly <ekellysnoco@gmail.com>
2024-12-13 19:11:05 -0800

    Fixed audio device saving and recalling from save

--- scripts/Audio/global_audio_manager.gd
+++ scripts/Audio/global_audio_manager.gd
@@ -22,28 +22,34 @@ var config_file_path = "user://config.cfg"
 # Reference the output device OptionButton
 @onready var output_device_selector: OptionButton = $"Audio Menu Constraints/Audio Settings/Output Device/Output Device Selector"
 
-## Sets the audio buses to a default value. Recalls settings from a saved config file.
+## Recalls audio settings from the config file. If there is no config file found, sets the audio buses to a default value.
 func _ready() -> void:
+	populate_audio_device_selector()
+	
 	if config.load(config_file_path) != OK:
+		print("The file \"" + config_file_path + "\" could not be loaded.\nThis could be your first playthrough or your local files are corrupted.")
 		set_master_volume(0.7)
 		set_music_volume(0.7)
 		set_sfx_volume(0.7)
 		set_ambience_volume(0.7)
 	
-	initialize_audio_device_selector()
+	# Read the config file for any changes to the active audio output before selecting
 	read_config_file()
+	select_active_audio_device()
 	
 	#TODO: play menu music here to avoid clicks
 	
 	#TODO: remove test audio 
-	$"TEST MUSIC".play()
+	$"TEMP MUSIC".play()
 
-## Sets up the audio device selector. Separated into its own function to avoid merge conflicts. 
-func initialize_audio_device_selector():
+## Parse the AudioServer's reported audio devices to populate a list 
+func populate_audio_device_selector():
 	# For each output device: Add it to the list
 	for device in AudioServer.get_output_device_list():
 		output_device_selector.add_item(device)
 
+## Parse the list of audio devices to locate the currently active device and select it. 
+func select_active_audio_device():
 	# For each item in the list: 
 	for i in range(output_device_selector.item_count):
 		# Check: is it the active device
@@ -53,8 +59,9 @@ func initialize_audio_device_selector():
 			output_device_selector.select(i) 
 			break
 
+## Saves audio bus volumes to a config file based on the user's preferences. 
 func save_audio_settings() -> void:
-	# Set values based on the user's preferences:
+	config.set_value("AudioSettings","output_device",AudioServer.output_device)
 	config.set_value("AudioSettings","master_volume",master_volume)
 	config.set_value("AudioSettings","music_volume",music_volume)
 	config.set_value("AudioSettings","sfx_volume",sfx_volume)
@@ -63,13 +70,15 @@ func save_audio_settings() -> void:
 	# Save to a file (or overwrite an existing file)
 	config.save(config_file_path)
 
+## Loads audio settings data from the config file on disk. 
 func read_config_file() -> void:
 	# Load data from the file
 	var err = config.load(config_file_path)
 	
 	# If the file didn't load, send a message and ignore it
 	if err != OK:
-		print("The file \"" + config_file_path + "\" could not be loaded.")
+		print("The file \"" + config_file_path + "\" could not be loaded.\n
+		This could be your first playthrough or your local files are corrupted.")
 		return
 	
 	# Parse the audio section and recall the stored volume settings. 
@@ -79,6 +88,7 @@ func read_config_file() -> void:
 	ambience_volume = config.get_value("AudioSettings","ambience_volume")
 	
 	# Set the audio settings according to recall results
+	AudioServer.output_device = config.get_value("AudioSettings","output_device")
 	set_master_volume(master_volume)
 	set_music_volume(music_volume)
 	set_sfx_volume(sfx_volume)
@@ -86,6 +96,7 @@ func read_config_file() -> void:
 
 ## DEBUG: print audio bus volumes
 func print_audio_settings() -> void:
+	print("Audio Device: \t" + AudioServer.output_device)
 	print("Master vol: \t" + str(master_volume))
 	print("Music vol: \t\t" + str(music_volume))
 	print("SFX vol: \t\t" + str(sfx_volume))
@@ -97,6 +108,9 @@ func _on_output_device_selector_item_selected(index: int):
 	var device = output_device_selector.get_item_text(index)
 	# Set to active output device
 	AudioServer.output_device = device 
+	await get_tree().create_timer(.1).timeout # Timer necessary to ensure proper saving of audio settings
+	on_audio_settings_changed()
+	
 
 ## Update the master bus volume, update the settings slider value, and save settings
 func set_master_volume(new_volume: float):
@@ -130,9 +144,11 @@ func set_ambience_volume(new_volume: float):
 	ambience_volume_slider.value = ambience_volume
 	on_audio_settings_changed()
 
+
+## Save audio settings whenever an audio setting is changed. 
 func on_audio_settings_changed() -> void: 
 	save_audio_settings()
-	#print_audio_settings() # DEBUG: reports audio settings via console
+	print_audio_settings() # DEBUG: reports audio settings via console
 
 
 func _on_master_volume_slider_value_changed(value: float) -> void:
