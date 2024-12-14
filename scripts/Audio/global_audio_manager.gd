83999faed6af06c649aaf5117a50a26a90e60ab6
Ethan Kelly <ekellysnoco@gmail.com>
2024-12-13 18:02:28 -0800

    Connected sliders, buttons to AudioServer functionality

--- scripts/Audio/global_audio_manager.gd
+++ scripts/Audio/global_audio_manager.gd
@@ -13,13 +13,14 @@ var config_file_path = "user://config.cfg"
 @export var sfx_bus_index = AudioServer.get_bus_index("SFX")
 @export var ambience_bus_index = AudioServer.get_bus_index("Ambience")
 
-# TODO: Reference volume settings sliders 
-#@onready var master_volume_slider: HSlider = $"MarginContainer/HBoxContainer/Settings Menu/Master Volume Slider"
-#@onready var music_volume_slider: HSlider = $"MarginContainer/HBoxContainer/Settings Menu/Music Volume Slider"
-#@onready var sfx_volume_slider: HSlider = $"MarginContainer/HBoxContainer/Settings Menu/SFX Volume Slider"
-#@onready var ambience_volume_slider: HSlider = $"MarginContainer/HBoxContainer/Settings Menu/Ambience Volume Slider"
+# Reference volume sliders
+@onready var master_volume_slider: HSlider = $"Audio Menu Constraints/Audio Settings/Master Volume/Master Volume Slider"
+@onready var music_volume_slider: HSlider = $"Audio Menu Constraints/Audio Settings/Music Volume/Music Volume Slider"
+@onready var sfx_volume_slider: HSlider = $"Audio Menu Constraints/Audio Settings/SFX Volume/SFX Volume Slider"
+@onready var ambience_volume_slider: HSlider = $"Audio Menu Constraints/Audio Settings/Ambience Volume/Ambience Volume Slider"
 
-@onready var output_device_options: OptionButton = $"Output Device Options"
+# Reference the output device OptionButton
+@onready var output_device_selector: OptionButton = $"Audio Menu Constraints/Audio Settings/Output Device/Output Device Selector"
 
 ## Sets the audio buses to a default value. Recalls settings from a saved config file.
 func _ready() -> void:
@@ -35,15 +36,15 @@ func _ready() -> void:
 func initialize_audio_device_selector():
 	# For each output device: Add it to the list
 	for device in AudioServer.get_output_device_list():
-		output_device_options.add_item(device)
+		output_device_selector.add_item(device)
 
 	# For each item in the list: 
-	for i in range(output_device_options.item_count):
+	for i in range(output_device_selector.item_count):
 		# Check: is it the active device
-		var device = output_device_options.get_item_text(i) 
+		var device = output_device_selector.get_item_text(i) 
 		if device == AudioServer.output_device:
 			# If it is, select the device. 
-			output_device_options.select(i) 
+			output_device_selector.select(i) 
 			break
 
 func save_audio_settings() -> void:
@@ -84,59 +85,58 @@ func print_audio_settings() -> void:
 	print("SFX vol: \t\t" + str(sfx_volume))
 	print("Ambience vol: \t" + str(ambience_volume))
 
+## When a device is selected from the menu, set the output device appropriately. 
+func _on_output_device_selector_item_selected(index: int): 
+	# Find device at index
+	var device = output_device_selector.get_item_text(index)
+	# Set to active output device
+	AudioServer.output_device = device 
+
 ## Update the master bus volume, update the settings slider value, and save settings
 func set_master_volume(new_volume: float):
 	master_volume = new_volume
 	var volume = linear_to_db(new_volume)
 	AudioServer.set_bus_volume_db(master_bus_index, volume)
-	# master_volume_slider.value = master_volume
+	master_volume_slider.value = master_volume
 	on_audio_settings_changed()
 
-
+## Update the music bus volume, update the settings slider value, and save settings
 func set_music_volume(new_volume: float):
 	music_volume = new_volume
 	var volume = linear_to_db(new_volume)
 	AudioServer.set_bus_volume_db(music_bus_index, volume)
-	# music_volume_slider.value = music_volume
+	music_volume_slider.value = music_volume
 	on_audio_settings_changed()
 
-## TODO: Connect this function to the signal for selecting an item from the output device dropdown menu
-func _on_output_device_options_item_selected(index: int): 
-	# Find device at index
-	var device = output_device_options.get_item_text(index)
-	# Set to active output device
-	AudioServer.output_device = device 
-	
+## Update the SFX bus volume, update the settings slider value, and save settings
 func set_sfx_volume(new_volume: float):
 	sfx_volume = new_volume
 	var volume = linear_to_db(new_volume)
 	AudioServer.set_bus_volume_db(sfx_bus_index, volume)
-	# sfx_volume_slider.value = sfx_volume
+	sfx_volume_slider.value = sfx_volume
 	on_audio_settings_changed()
 
+## Update the ambience bus volume, update the settings slider value, and save settings
 func set_ambience_volume(new_volume: float):
 	ambience_volume = new_volume
 	var volume = linear_to_db(new_volume)
 	AudioServer.set_bus_volume_db(ambience_bus_index, volume)
-	# ambience_volume_slider.value = ambience_volume
+	ambience_volume_slider.value = ambience_volume
 	on_audio_settings_changed()
 
 func on_audio_settings_changed() -> void: 
 	save_audio_settings()
-	# print_audio_settings() # DEBUG: reports audio settings via console
+	print_audio_settings() # DEBUG: reports audio settings via console
 
-# TODO: Link sliders to volume settings on value changed
-#func _on_master_volume_slider_value_changed(value: float) -> void:
-	#set_master_volume(value)
-#
-#
-#func _on_music_volume_slider_value_changed(value: float) -> void:
-	#set_music_volume(value)
-#
-#
-#func _on_sfx_volume_slider_value_changed(value: float) -> void:
-	#set_sfx_volume(value)
-#
-#
-#func _on_ambience_volume_slider_value_changed(value: float) -> void:
-	#set_ambience_volume(value)
+
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	set_master_volume(value)
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	set_music_volume(value)
+
+func _on_sfx_volume_slider_value_changed(value: float) -> void:
+	set_sfx_volume(value)
+
+func _on_ambience_volume_slider_value_changed(value: float) -> void:
+	set_ambience_volume(value)
